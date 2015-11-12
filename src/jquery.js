fd7c5fd0145e1c27169e12a045b4f63089a014c3
ALRW <awerner1@googlemail.com>
2015-11-12 14:21:24 +0000

    Added Weather API

--- src/jquery.js
+++ src/jquery.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
 
+  var showExtTemperature = function(resp){
+    var tempInC = _convertKtoC(resp.main.temp);
+    $('#outsideTemp').text(tempInC);
+  };
+
+  var updateLocalWeatherDisplay = function(location){
+    var url = "http://api.openweathermap.org/data/2.5/weather";
+    var city = "q=" + location;
+    var appID = "appid=0fe786c5ab2f53ba371796181af553b4";
+    var data = city + "&" + appID;
+    $.getJSON(url, data, showExtTemperature);
+  };
+
+  var _convertKtoC = function (kelvins){
+    return Math.round(kelvins - 273.15);
+  };
+
   var updateDisplay = function(){
     $('#temp').text(thermostat.temperature());
     _updatePSMStatus();
@@ -25,6 +42,13 @@ $(document).ready(function(){
 
   updateDisplay();
 
+  updateLocalWeatherDisplay($('#Location').val());
+
+  $('#locForm').submit(function(event){
+    var location = $('#Location').val();
+    updateLocalWeatherDisplay(location);
+    event.preventDefault();
+  });
   $('#reset').on('click',function(){
     thermostat.resetButton();
     updateDisplay();
